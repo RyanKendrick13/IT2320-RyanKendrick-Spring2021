188b40e04aee0f3d182be3c3b3606f543eb714a1
RyanKendrick13 <62944299+RyanKendrick13@users.noreply.github.com>
2021-03-15 21:14:53 -0400

    Add files via upload

--- Lab 6/jslab6.js
+++ Lab 6/jslab6.js
@@ -1,7 +1,7 @@
 /*********************************************************************/
 //Global Area
 
-createLink();
+
 
 
 /***************** Function definition area *************/
@@ -10,8 +10,67 @@ createLink();
 function createLink(){
     var parent = document.getElementById("parentLink");
     var child=document.createElement("a");
-    child.href="www.tri-c.edu";
+child.href="https://www.tri-c.edu";
+child.innerHTML="www.tri-c.edu";
+
+   
+
     parent.appendChild(child);
-    
+
+
+}
+
+function removeLink()
+{
+var parent = document.getElementById("parentLink");
+var child = parent.lastElementChild;
+parent.removeChild(child);
+
+
+
+}
+function changeSquares()
+{
+var square1 = document.getElementsByClassName("square1");
+        var square2 = document.getElementsByClassName("square2");
+for(var i=0; i<square1.length;i++)
+    {
+        square1[i].style.backgroundColor = "black";
+square1[i].style.color = "white";
+
+       
+
+    }
+for(var i=0; i<square2.length;i++)
+{
+square2[i].style.backgroundColor = "white";
+square2[i].style.color = "black";
+        }
+
+
+}
+
+function changeShape()
+
+{
+var radius = document.getElementById("inputRadius").value;
+        var square1 = document.getElementsByClassName("square1");
+for(var i=0; i<square1.length;i++)
+    {
+        square1[i].style.borderRadius = radius;
+       
+
+    }
+
+
+
+}
+
+function changeColor()
+{
+var inputColor = document.getElementById("inputColor").value;
+var box = document.getElementById("box");
+box.style.backgroundColor=inputColor;
+
 
 }
