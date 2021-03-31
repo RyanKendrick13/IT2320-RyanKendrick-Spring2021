74a3e8b2f8998ef265328503fdad46fe2068abf7
RyanKendrick13 <62944299+RyanKendrick13@users.noreply.github.com>
2021-03-31 12:29:19 -0400

    Add files via upload

--- Mini Project#2/script.js
+++ Mini Project#2/script.js
@@ -1,46 +1,114 @@
 //Global area
+var winningPos=[[0,1,2],
+		[3,4,5],
+		[6,7,8],
+		[0,3,6],
+		[2,5,8],
+		[0,4,8],
+		[2,4,6]];
 window.onload=function() {
 //Try and use classname to get a group of divs instead of getting them one by one by id
 //if you are writing the same type of code multiple times, you are doing it incorrect
 
 //Attach events using eventlistener here
+var squares= document.getElementsByClassName("col");
 
-
-var x= document.getElementsByClassName("col");
-for(var i=0;i<x.length;i++)
+for(var i=0; i<squares.length;i++)
 {
-x[i].addEventListener("click",function(){
-    userTurn(this);
+	squares[i].addEventListener("click",function(){
+	userTurn(this);
+
 });
 }
-var square= document.getElementsByClassName("col");
+
+
+
+
+
+
+
+
+
 /************** Function area *******************/
 
 //function provided. Add parameter(s) if needed
-function userTurn(x)
+function userTurn(square)
 {
     //Add code here
-    x.innerHTML="X";
-    computerTurn();
+	
+
+	 square.innerHTML="X";
+	computerTurn();
+	DetermineWinner(square);
+	
+	
+
+ 
+
+	
+
 }
 
 //function provided. Add parameter(s) if needed
 function computerTurn()
 {
     //Add code here
+for(var i=0;i<squares.length;i++)
+{
 
-    for(var i =0;i<square.length;i++)
-    {
-       if(square[i].innerHTML==!"X"||square[i].innerHTML==!"O")
-       {
-        square[i].innerHTML="O";
-        break;
-       }
-       else
-       {
-           continue;
-       }
-    }
+	if(squares[i].innerHTML==!"O"||squares[i].innerHTML==!"X")
+	
+
+{
+	squares[i].innerHTML="O";
+	DetermineWinner(squares);
+	break;
+		
+}
+
+	else
+{
+	continue;
+	
+}
+}
+
+	
+
+}
+
+
+
+
+	function DetermineWinner(elem)
+{
+    var squares= document.getElementsByClassName("col");
+
+	for(var i=0;i<=winningPos.length;i++)
+
+{
+    console.log(winningPos[i][0]);
+    console.log(winningPos[i][1]);
+    console.log(winningPos[i][2]);
+	/*if(elem[i]==winningPos[i])
+{
+	var message = document.getElementById("message");
+	message.innerHTML="Computer is the winner";
+	
+}
+	else if(elem[i]==winningPos[i])
+{
+	var message = document.getElementById("message");
+	message.innerHTML="User is the winner";
+	
+}
+	else
+{
+	break;
+}
+*/
+}
+
 
 }
 }
