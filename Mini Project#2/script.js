584106c88c3dbb51c56e09945772f7451b12834e
RyanKendrick13 <62944299+RyanKendrick13@users.noreply.github.com>
2021-04-04 19:17:35 -0400

    Add files via upload

--- Mini Project#2/script.js
+++ Mini Project#2/script.js
@@ -5,13 +5,18 @@ var winningPos=[[0,1,2],
 		[0,3,6],
 		[2,5,8],
 		[0,4,8],
-		[2,4,6]];
+		[2,4,6],
+        [1,4,7]];
+var win = false;
+        
 window.onload=function() {
 //Try and use classname to get a group of divs instead of getting them one by one by id
 //if you are writing the same type of code multiple times, you are doing it incorrect
 
 //Attach events using eventlistener here
 var squares= document.getElementsByClassName("col");
+var reset=document.getElementById("reset");
+reset.addEventListener("click",ResetGameBoard);
 
 for(var i=0; i<squares.length;i++)
 {
@@ -36,11 +41,14 @@ function userTurn(square)
 {
     //Add code here
 	
-
-	 square.innerHTML="X";
-	computerTurn();
+    if((square.innerHTML==!"O")||(square.innerHTML==!"X")){
+	 if(win==false){
+    square.innerHTML="X";
+     computerTurn();
 	DetermineWinner(square);
-	
+    
+}
+	}
 	
 
  
@@ -80,35 +88,47 @@ for(var i=0;i<squares.length;i++)
 
 
 
-	function DetermineWinner(elem)
+function DetermineWinner(elem)
 {
     var squares= document.getElementsByClassName("col");
+    
 
-	for(var i=0;i<=winningPos.length;i++)
+for(var i=0;i<winningPos.length;i++)
 
 {
-    console.log(winningPos[i][0]);
-    console.log(winningPos[i][1]);
-    console.log(winningPos[i][2]);
-	/*if(elem[i]==winningPos[i])
-{
-	var message = document.getElementById("message");
-	message.innerHTML="Computer is the winner";
-	
-}
-	else if(elem[i]==winningPos[i])
-{
-	var message = document.getElementById("message");
+if((squares[winningPos[i][0]].innerHTML=="X")&&(squares[winningPos[i][1]].innerHTML=="X")&&(squares[winningPos[i][2]].innerHTML=="X")){
+    var message = document.getElementById("message");
 	message.innerHTML="User is the winner";
+    win=true;
 	
 }
-	else
-{
-	break;
+
+    if((squares[winningPos[i][0]].innerHTML=="O")&&(squares[winningPos[i][1]].innerHTML=="O")&&(squares[winningPos[i][2]].innerHTML=="O")){
+        var message = document.getElementById("message");
+        message.innerHTML="Computer is the winner";
+		win = true;
+        
+    }
 }
-*/
-}
+} 
+
+ function ResetGameBoard(){
+
+	var squares= document.getElementsByClassName("col");
+	document.getElementById("status").innerHTML="This is a new game!!! Click on any of the squares to start the game";
+	document.getElementById("message" ).innerHTML=" ";
+	win=false;
+	
+
+	for(var i=0;i<squares.length;i++)
+	{
+		squares[i].innerHTML="";
+		
+
+	}
 
 
 }
+
 }
+
