47bae3e29486d555414c8839e7805a9875348a33
RyanKendrick13 <62944299+RyanKendrick13@users.noreply.github.com>
2021-04-06 08:49:50 -0400

    Add files via upload

---
+++ Lab 8/json.js
@@ -0,0 +1,9 @@
+var book={title:"Mastery",author:"Robert Greene",totalpages:318,publisher:"Viking Press",ISBN:9780670024964};
+
+
+var JSON=JSON.stringify(book);
+
+
+var employee={"firstname":"Brad ","lastname":"Smith","ID":1234,"jobtitle":"Ethical Hacker"};
+
+var emp=JSON.parse("firstname":"Brad ","lastname":"Smith","ID":1234,"jobtitle":"Ethical Hacker");
